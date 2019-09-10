52fe4b7e3f4eba1e9be6e4093ad09dc85efe0594
Arnor <arnor0903@gmail.com>
2019-09-10 16:56:53 +0000

    404 page og fleiri improvements

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 import random
 from flask import Flask, render_template as rend
 app = Flask(__name__)
-frettir = {"1":["yeet", "maður yeetaði"]}
+frettir = {
+	"1":["yeet", "maður yeetaði"],
+	"2":[]
+	}
 @app.route('/')
 def index():
-	return rend("base.html", title="index") + '<h1>oh hi there</h1>'
+	return rend("frettir.html", title="index", frettir=frettir.keys())
 @app.route("/<frett>")
 def frett(frett):
-	return rend("base.html", title=frettir[frett][0], content=frettir[frett][1])
+	if frett in frettir.keys():
+		return rend("frett.html", title=frettir[frett][0], content=frettir[frett][1])
+	else:
+		return page_not_found(None)
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return rend('404.html'), 404
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
