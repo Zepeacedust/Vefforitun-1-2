55396d56ab29a2ee17225d6a02498ba1abcf4b7f
Arnor <arnor0903@gmail.com>
2019-09-10 17:25:34 +0000

    burglary

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
-import random
+import random, ast
 from flask import Flask, render_template as rend
 app = Flask(__name__)
-frettir = {
-	"1":["yeet", "maður yeetaði"],
-	"2":[]
-	}
+with open("frettir.txt") as frett_file:
+	frettir = ast.literal_eval(frett_file.read())
 @app.route('/')
 def index():
 	return rend("frettir.html", title="index", frettir=frettir.keys())
+
+
 @app.route("/<frett>")
 def frett(frett):
 	if frett in frettir.keys():
 		return rend("frett.html", title=frettir[frett][0], content=frettir[frett][1])
 	else:
 		return page_not_found(None)
+
+
 @app.errorhandler(404)
 def page_not_found(e):
-    # note that we set the 404 status explicitly
-    return rend('404.html'), 404
+    return rend('404.html')
 
 
 if __name__ == "__main__":
